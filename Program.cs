7742ad04395bdd33dfed62f3674864de12f06dad
Tobias Burns <toby8urns@gmail.com>
2023-02-10 15:39:27 -0500

    Updated Mud Drawer. Moved bottom appBar to be a socials component that is currently included at the bottom of drawer. Added bookwyrm and pixelfed logos. First full version with CV on front page.

--- Program.cs
+++ Program.cs
@@ -4,10 +4,14 @@ using Personal_Website;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddLogging(builder => builder
+                .SetMinimumLevel(LogLevel.Debug));
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// TODO: Replace this with the proper json service
+builder.Services.AddScoped<ICVService, JsonCVService>();
 
 await builder.Build().RunAsync();
